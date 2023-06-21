e8b64fddbb69a9cc2a9aa1dc861875962c964331
GuralTOO <guralben@gmail.com>
2023-06-21 10:31:12 -0400

    1

--- WeaviateClient.py
+++ WeaviateClient.py
@@ -146,8 +146,8 @@ def search_items(class_name, properties=[""], text_query="", k=10, path=""):
     properties.append("path")
     results = client.query.get(class_name=class_name, properties=properties).with_where({"path": "path", "operator": "beginsWith", "valueString": path}).with_near_text(
         {"concepts": text_query}).with_limit(k).do()
-    print("search results: ", results["data"]["Get"][class_name])
-    return results["data"]["Get"][class_name]
+    print("search results: ", results)
+    return results
 
 # return all items in a class
 
