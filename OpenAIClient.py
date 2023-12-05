57f65a756011556fd95db215fe305db2c8907559
GuralTOO <guralben@gmail.com>
2023-12-05 10:20:50 -0500

    update to openai client chat completions

--- OpenAIClient.py
+++ OpenAIClient.py
@@ -7,7 +7,7 @@ class_name = "File_store"
 def get_answer(query: str, path: str):
     context = WeaviateClient.search_items(class_name=class_name, properties=[
         "text"], text_query=query, k=5, path=path)
-    response = openai.chat.completion.create(
+    response = openai.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[{"role": "system", "content": "You are a helpful assistant that answers questions based on excerpts from the following documents:" + str(context)},
                   {"role": "user", "content": "This is my question: " + query}],
@@ -20,7 +20,7 @@ def get_answer(query: str, path: str):
 def get_answer_stream(question: str, path: str):
     context = WeaviateClient.search_items(class_name=class_name, properties=[
         "text"], text_query=question, k=5, path=path)
-    response = openai.chat.completion.create(
+    response = openai.chat.completions.create(
         model="gpt-4",
         messages=[{"role": "system", "content": "You are a helpful assistant that answers questions based on excerpts from the following documents:" + str(context)},
                   {"role": "user", "content": "This is my question: " + question}],
